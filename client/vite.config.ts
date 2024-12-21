7e7180c064ab419393057d03fd28d9167635dc81
Ubuntu <ubuntu@ip-172-31-12-106.ap-south-1.compute.internal>
2024-12-21 23:16:39 +0000

    change vite config file so that it can run on ec2

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Binds to all available network interfaces
+    port: 5173,        // Ensure the port matches your desired port (5173 in this case)
+  },
 })
+
